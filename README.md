2fda2b154d0ac8f90e45979c5ddd5cb34d84693b
pawelkleczek <pa.vel.kleczek@gmail.com>
2017-04-26 10:35:12 +0200

    Adding WikiViewer to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# WikipediaViewer
-Wikipedia Viewer - FreeCodeCamp challenge
+# Wikipedia Viewer API
+
+### FreeCodeCamp challenge
+#### by Paweł Kłeczek
+
+## What is this?
+
+Wikipedia Viewer API is a raw, straightforward tool for search querying Wikipedia for articles. Basically it uses javascript to construct a proper link. Wikipedia API in turn sends articles (that correspond to the query) in JSON format. Then it processes the data, so that user can get a list of articles with links to proper Wikipedia subpages.
+
+![alt tag](images/query_example.png)
+
+Check it out on CodePen:
+http://codepen.io/pawelkleczek/full/XpLZOq/
---
+++ js/app.js
@@ -0,0 +1,23 @@
+/* https://en.wikipedia.org/w/api.php?action=query&format=jsonfm&list=search&srsearch= */
+
+$( "form" ).submit(function( event ) {
+
+  $(".1").html("<br>");
+
+  var input = $("#duu").val();
+
+  var http = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + input + "&callback=?";
+
+
+ $.getJSON(http, function(json) {
+
+   for (var i = 0; i < 10; i++)
+     if (json.query.search[i] !== undefined)
+       $(".1").append("<p>Title: <a href='https://en.wikipedia.org/wiki/" + json.query.search[i].title + "' target='_blank'>" + json.query.search[i].title + "</a></p>" + "(...) " + json.query.search[i].snippet + "<br><br>");
+
+
+ });
+
+ event.preventDefault();
+
+});
